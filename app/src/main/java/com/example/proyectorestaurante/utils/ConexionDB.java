cbfaf4b827b6b1aadb988286a3541385475f8ead
BONIFAZ282 <bonihd48@gmail.com>
2023-10-13 14:30:19 -0500

    Se le cambio el diseño 1

--- app/src/main/java/com/example/proyectorestaurante/utils/ConexionDB.java
+++ app/src/main/java/com/example/proyectorestaurante/utils/ConexionDB.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 
 public class ConexionDB {
     private static final String DB_URL = "jdbc:jtds:sqlserver://192.168.56.1:1433/restaurante";
-    private static final String DB_USER = "sa";
+    private static final String DB_USER = "codel";
     private static final String DB_PASSWORD = "123";
 
     public static Connection obtenerConexion() {
